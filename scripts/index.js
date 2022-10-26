11e0778cab7aa7f7576101a736f352f1ff8f5802
Alexander Sokolovskiy <sokolovskiya12@gmail.com>
2022-10-26 15:56:17 -0700

    new week. sprint-3 Show page finished with API .get request. displaying everything correctly. page done.

--- scripts/index.js
+++ scripts/index.js
@@ -1,75 +1,70 @@
 
-//create array of objects containing comments information
-const commentsArray = [{ 
-        name: 'Connor Walton',
-        date: '02/17/2021',
-        comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-    },
-    { 
-        name: 'Emilie Beach',
-        date: '01/09/2021',
-        comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-    },
-    { 
-        name: 'Miles Acosta',
-        date: '12/20/2020',
-        comment: 'I can`t stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can`t get enough.',
-    },
-];
 
-//grab the container for comment section
-const commentSection = document.querySelector(".comments")
-    
+axios
+    .get("https://project-1-api.herokuapp.com/comments?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4")
+    .then(response => {
+    console.log(response);
+    let commentArray = response.data;
+    console.log(commentArray);
+
+    const comments = document.querySelector(".comments");
+
+    const commentsContainer = document.createElement('div');
+    commentsContainer.classList.add("comments__container");
+    comments.appendChild(commentsContainer);
+
+
 //create container for comments and append it to comment section
-const comments = document.createElement('div');
-comments.classList.add("comments__container");
-commentSection.appendChild(comments);
-
-//grab comment form from by id
-const commentForm = document.getElementById ('commentForm');
-
-
-//create a comment and by grabbing matching values from array of objects
-function displayComment (commentsArray) {
+    function displayComment (commentArray) {
+        const dateOfcomment = new Date(commentArray.timestamp);
+        var dateFormat = {
+            year: "numeric",
+            month: "2-digit",
+            day: "numeric"
+        };
+        const commentArticle = document.createElement("article");
+        commentArticle.classList.add ("comments__article");
+        commentsContainer.appendChild(commentArticle);
     
-    const commentArticle = document.createElement("article");
-    commentArticle.classList.add ("comments__article");
-    comments.appendChild(commentArticle);
+        const avoDiv = document.createElement ("div");
+        avoDiv.classList.add ("comments__avo");
+        commentArticle.appendChild(avoDiv);
+    
+        const commentBox = document.createElement ("div");
+        commentBox.classList.add ("comments__comment");
+        commentArticle.appendChild(commentBox);
+    
+        const commentName = document.createElement ("p");
+        commentName.classList.add("comments__name");
+        commentBox.appendChild(commentName);
+        commentName.innerText = commentArray.name;
+    
+        const commentDate = document.createElement ("p");
+        commentDate.classList.add("comments__date");
+        commentBox.appendChild(commentDate);
+        commentDate.innerText = dateOfcomment.toLocaleDateString("en", dateFormat);
+    
+        const commentText = document.createElement ("p");
+        commentText.classList.add("comments__text");
+        commentBox.appendChild(commentText);
+        commentText.innerText = commentArray.comment;
+    } 
+    for (let i = 0; i<commentArray.length; i++){
+        displayComment(commentArray[i])
+    };
+});
 
-    const avoDiv = document.createElement ("div");
-    avoDiv.classList.add ("comments__avo");
-    commentArticle.appendChild(avoDiv);
-
-    const commentBox = document.createElement ("div");
-    commentBox.classList.add ("comments__comment");
-    commentArticle.appendChild(commentBox);
-
-    const commentName = document.createElement ("p");
-    commentName.classList.add("comments__name");
-    commentBox.appendChild(commentName);
-    commentName.innerText = commentsArray.name;
-
-    const commentDate = document.createElement ("p");
-    commentDate.classList.add("comments__date");
-    commentBox.appendChild(commentDate);
-    commentDate.innerText = commentsArray.date;
-
-    const commentText = document.createElement ("p");
-    commentText.classList.add("comments__text");
-    commentBox.appendChild(commentText);
-    commentText.innerText = commentsArray.comment;
-}   
-
-//creating an event 'submit' from the form to set up action on thу click
+/*
+const commentForm = document.getElementById ('commentForm');
 commentForm.addEventListener ('submit', function(event) {
-    //grab text and commetn field by id
+    event.preventDefault();
+    
     let nameField = document.querySelector('#name-text')
     let commentField = document.querySelector('#comment-text')
-    //clear up the commetn section
-    comments.innerText = '';
+    commentsContainer.innerText = '';
 
-    // 
-    if ((nameField.value === '' || nameField.value == null) || (commentField.value === '' || commentField.value == null)) {
+    if ((nameField.value === '' || nameField.value == null) || 
+        (commentField.value === '' || commentField.value == null)) {
         event.preventDefault();
         console.log("form is empty, Please fill-out the form");
         nameField.classList.add('error');
@@ -91,17 +86,134 @@ commentForm.addEventListener ('submit', function(event) {
             date: dateNow.toLocaleDateString("en", dateFormat),
             comment: event.target.comment.value
         };
-        commentsArray.unshift(newCommentEntry);
+        commentArray.unshift(newCommentEntry);
     } 
-    for (let i = 0; i < commentsArray.length; i++) {
-        displayComment(commentsArray[i]);
+    for (let i = 0; i < commentArray.length; i++) {
+        displayComment(commentArray[i]);
     };
     nameField.value = '';
     commentField.value = '';
+    });
+    
+*/
+
+
+
+
+
+/*
+    btn.addEventListener("click", () => {
+    const name = nameInput.value;
+    const comment = commentInput.value;
+
+    axios.post("https://reqres.in/api/login", {
+        email: email,
+        password: password
+    })
+    .then((response) => {
+        console.log(response);
+    });
 });
-
-for (let i = 0; i < commentsArray.length; i++) {
-    displayComment(commentsArray[i]);
-};
+*/
 
 
+    
+/*
+// loop  through array of objects and append to journal-entries
+
+// lForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const title = event.target.title.value;
+//     const content = event.target.content.value;
+//     if (title == '') {
+//       console.log('title is empty');
+//       return;
+//     }
+//     journalEntriesArray.push({
+//       title,
+//       content,
+//     });
+//     renderJournalEntries(); // loop  through array of objects and append to journal-entries
+//   });
+//commentsData(); //call the function so it doesnt reset with "";
+
+
+// const imgElement = document.createElement("img");
+// imgElement.classList.add('comment-fetched__img')
+// imgElement.setAttribute('src="./assets/Images/avatarMercury.png"');
+// function addComments(listObj) {
+//     const card = document.createElement("div");
+//     card.classList.add()
+
+    
+// }
+
+
+// const form = document.querySelector('.comment-form');
+// console.log(form);
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault
+//     console.log(event.target.name.value);
+//     console.log(event.target.comment.value);
+//     //pull the value from "name"
+
+// });
+
+
+
+
+// function addCardToMenu(menuObj) {
+//     const card = document.createElement("div"); //creates new element div
+//     card.classList.add("menu__card"); //assignes class to div created 
+    
+//     const item = document.createElement("div"); 
+//     item.classList.add("menu__item");
+//     item.innerText = menuObj.icon;
+//     const price = document.createElement("div");
+//     price.classList.add("menu__price");
+//     price.innerText = `$ ${menuObj.price}`;
+    
+//     card.appendChild(item); //says that item is a direct child of card 
+//     card.appendChild(price); //says that price is a direct child of card 
+//     const menuElement = document.querySelector(".menu") //
+//     menuElement.appendChild(card);
+
+    
+// }
+
+// item.addEventListener("click", function (event) {
+//     const cartItemsContainer = document.querySelector(".cart__items");
+//     const cartElement = document.createElement("div"); //creates new element div
+//     cartElement.classList.add("cart__item"); //assignes class to div created  
+//     console.log(event.target);
+//     cartElement.innerText = event.target.innerText;
+//     cartItemsContainer.appendChild(cartElement);
+// })    
+
+
+
+// for (let i=0; i<menuItems.length; i++){
+//     addCardToMenu(menuItems[i])
+// }
+// const conversationSection = [];
+// //function buttonHandler
+// const button = document.querySelector('.button');
+// console.log(button); //to make sure we are getting the right elemente
+// button.addEventListener('click', function(){
+//     console.log('btn clicked')//to make sure is working,console triggered when clicked 
+// }); //whenever we click this button we can have a function to run
+//mouseover
+
+//mouseout
+//const imgDiv = document.createAttribute('div');
+    //imgDiv.classList.add('comment-fetched__img-container')
+
+    //const imgElement = document.createElement("img");
+    //imgElement.classList.add('comment-fetched__img');
+    //mgElement.setAttribute('src="./assets/Images/avatarMercury.png"');
+    // create parent element
+    //const box = document.createElement('div');
+    // assign your css classes
+    //box.classList.add('comment-fetched__box');
+*/
--- scripts/shows.js
+++ scripts/shows.js
@@ -1,107 +1,79 @@
 
 const labels = ['date', 'venue', 'location'];
-const futureShows = [{ 
-        date: 'Mon Sept 06 2021',
-        venue: 'Roland Lane',
-        location: 'San Francisco, CA',
-    },
-    { 
-        date: 'Tue Sept 21 2021',
-        venue: 'Pier 3 East',
-        location: 'San Francisco, CA',
-    },
-    { 
-        date: 'Fri Oct 15 2021',
-        venue: 'View Lounge',
-        location: 'San Francisco, CA',
-    },
-    { 
-        date: 'Sat Nov 06 2021',
-        venue: 'Hyatt Agency',
-        location: 'San Francisco, CA',
-    },
-    { 
-        date: 'Fri Nov 26 2021',
-        venue: 'Press Club',
-        location: 'San Francisco, CA',
-    },
-    { 
-        date: 'Wed Dec 15 2021',
-        venue: 'Press Club',
-        location: 'San Francisco, CA',
-    },
-];
+
+axios.get("https://project-1-api.herokuapp.com/showdates?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4").then(response => {
+    let futureShows = response.data;
     
-const showsContainer = document.querySelector('.shows__container');
-
-
-//creating 3 <p> elements with class 'label' and nesting it within showsLabelDiv
-
-function addShows (featureShows) {
-    
-    const showsShow = document.createElement("div");
-    showsShow.classList.add("shows__show");
-    showsContainer.appendChild(showsShow);
-
-    const labelDate = document.createElement ('p');
-    labelDate.classList.add('label','shows__label');
-    showsShow.appendChild(labelDate);
-    labelDate.innerText = labels[0];
-
-    const showDate = document.createElement ('p');
-    showDate.classList.add('shows__date');
-    showsShow.appendChild(showDate);
-    showDate.innerText = featureShows.date;
-
-    const labelVenue = document.createElement ('p');
-    labelVenue.classList.add('label','shows__label');
-    showsShow.appendChild(labelVenue);
-    labelVenue.innerText = labels[1];
-
-    const showVenue = document.createElement ('p');
-    showVenue.classList.add('shows__venue');
-    showsShow.appendChild(showVenue);
-    showVenue.innerText = featureShows.venue;
-
-    const labelLocation = document.createElement ('p');
-    labelLocation.classList.add('label','shows__label');
-    showsShow.appendChild(labelLocation);
-    labelLocation.innerText = labels[2];
-
-    const showLocation = document.createElement ('p');
-    showLocation.classList.add('shows__location');
-    showsShow.appendChild(showLocation);
-    showLocation.innerText = featureShows.location;
-
-    const button = document.createElement("button");
-    button.classList.add('button-link');
-    button.innerText = 'buy tickets';
-    showsShow.appendChild(button);
-};
-
-for (let i = 0; i<futureShows.length; i++){
-    addShows(futureShows[i])
-};
-
-
-var show = document.querySelectorAll('.shows__show');
-console.log(show);
-
-show.forEach
-
-
-        /*(el=> {
-    el.addEventListener('click', function(show){
+    const showsContainer = document.querySelector('.shows__container');
+    //creating 3 <p> elements with class 'label' and nesting it within showsLabelDiv
+    function addShows (featureShows) {
+        const dateOfShow = new Date(featureShows.date);
         
-        el.classList.add('active');
-    });
-});*/
+        var dateFormat = {
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "numeric"
+        };
+        //console.log(dateOfShow.toLocaleDateString("en".dateFormat));
 
-show.forEach(function(item) {
-    item.addEventListener("click", function() {
-        show.forEach(function(element) {
-        element.classList.remove("active");
-        });
-        item.classList.add('active');
-    })
-});
+        console.log(dateOfShow.toLocaleDateString("en", dateFormat));
+
+        const showsShow = document.createElement("div");
+        showsShow.classList.add("shows__show");
+        showsContainer.appendChild(showsShow);
+    
+        const labelDate = document.createElement ('p');
+        labelDate.classList.add('label','shows__label');
+        showsShow.appendChild(labelDate);
+        labelDate.innerText = labels[0];
+    
+        const showDate = document.createElement ('p');
+        showDate.classList.add('shows__date');
+        showsShow.appendChild(showDate);
+        showDate.innerText = dateOfShow.toLocaleDateString("en", dateFormat);
+    
+        const labelVenue = document.createElement ('p');
+        labelVenue.classList.add('label','shows__label');
+        showsShow.appendChild(labelVenue);
+        labelVenue.innerText = labels[1];
+    
+        const showVenue = document.createElement ('p');
+        showVenue.classList.add('shows__venue');
+        showsShow.appendChild(showVenue);
+        showVenue.innerText = featureShows.place;
+    
+        const labelLocation = document.createElement ('p');
+        labelLocation.classList.add('label','shows__label');
+        showsShow.appendChild(labelLocation);
+        labelLocation.innerText = labels[2];
+    
+        const showLocation = document.createElement ('p');
+        showLocation.classList.add('shows__location');
+        showsShow.appendChild(showLocation);
+        showLocation.innerText = featureShows.location;
+    
+        const button = document.createElement("button");
+        button.classList.add('button-link');
+        button.innerText = 'buy tickets';
+        showsShow.appendChild(button);
+    };
+    
+    for (let i = 0; i<futureShows.length; i++){
+        addShows(futureShows[i])
+    };
+    
+    var show = document.querySelectorAll('.shows__show');
+    console.log(show);
+    
+    show.forEach
+    
+    show.forEach(function(item) {
+        item.addEventListener("click", function() {
+            show.forEach(function(element) {
+            element.classList.remove("active");
+            });
+            item.classList.add('active');
+        })
+    });
+    });
